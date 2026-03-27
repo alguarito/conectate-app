8c47bb4c76f75031af32af42aec6218dff6fbeb6
Alvaro Cardenas Orozco <alvarocardenasorozco@Alvaros-MacBook-Air.local>
2026-03-27 07:14:18 -0500

    UX Fix v1.8.2: Service Worker v5 y Cache Busting final

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'conectate-cache-v4-master'; // Nueva versión para forzar actualización
+const CACHE_NAME = 'conectate-cache-v5-master'; // Nueva versión para forzar actualización
 const urlsToCache = [
   './',
   './index.html',
